4c97c1287cb7c2795d3f08d71cac0c9325126842
Arnel <arnelcariaga@hotmail.com>
2024-09-16 00:56:09 -0400

    Adding filtering functionality

--- components/RolesElement/rolesColumns.tsx
+++ components/RolesElement/rolesColumns.tsx
@@ -6,22 +6,22 @@ import { Button } from "@/components/ui/button"
 import { IRolesScreens } from "@/lib/interfaces"
 import { formatDistanceToNow } from "date-fns";
 import { es } from "date-fns/locale";
+import { TSelectedRolObj } from "@/lib/types";
 
-export const rolesColumns = (openModalEditRol: (idRol:number) => void, openModalDeleteRol: (idRol:number) => void): ColumnDef<IRolesScreens>[] => [
+export const rolesColumns = (openModalEditRol: ({id, name}: TSelectedRolObj) => void, openModalDeleteRol: (idRol:number) => void): ColumnDef<IRolesScreens>[] => [
     {
         accessorKey: "name",
-        header: "Rol",
-        enableSorting: true,
+        enableColumnFilter: true,
+        header: "Rol"
     },
     {
         accessorKey: "screens",
         header: "Acceso a pantalla(s)",
-        enableSorting: true,
         cell: ({ row }) => {
             const screens = row.original.screens
             const is_admin = row.original.is_admin
 
-            return is_admin ? "Acceso a todas las pantallas y módulos" : screens.length === 0 ? "Sin acceso a pantalla o módulo" :
+            return is_admin ? "Acceso a todas las pantallas" : screens.length === 0 ? "Sin acceso a ninguna pantalla" :
                 screens.map(({ name }, i) => <p key={i}>{name}</p>)
         }
 
@@ -29,7 +29,6 @@ export const rolesColumns = (openModalEditRol: (idRol:number) => void, openModal
     {
         accessorKey: "updated_at",
         header: "Última actualización",
-        enableSorting: true,
         cell: ({row}) => {
             const datetime = formatDistanceToNow(new Date(row.original.updated_at), { addSuffix: true, locale: es });
             return datetime
@@ -40,9 +39,10 @@ export const rolesColumns = (openModalEditRol: (idRol:number) => void, openModal
         header: "Acciones",
         cell: ({ row }) => {
             const rolId = row.original.id
+            const rolName = row.original.name
             return (
                 <div className="space-x-2">
-                    <Button variant="secondary" onClick={() => openModalEditRol(rolId)}>Editar</Button>
+                    <Button variant="secondary" onClick={() => openModalEditRol({id: rolId, name: rolName})}>Editar</Button>
                     <Button variant="destructive" onClick={() => openModalDeleteRol(rolId)}>Eliminar</Button>
                 </div>
             )
