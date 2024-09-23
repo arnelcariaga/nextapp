13d1839de1f3f4488a1b3cdcaa3c6ce1e8800430
Arnel <arnelcariaga@hotmail.com>
2024-09-23 13:39:41 -0400

    SAIs slice

---
+++ redux/slices/saisSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface IInitialState {
+  closeModalAddSai: boolean
+  closeModalEditSai: boolean
+  addedSais: Array<object>
+}
+
+const initialState: IInitialState = {
+  closeModalAddSai: false,
+  closeModalEditSai: false,
+  addedSais: []
+};
+
+export const saisSlice = createSlice({
+  name: "sais",
+  initialState,
+  reducers: {
+    setCloseModalAddSai: (state, action) => {
+      const closeModalAddSai = action.payload;
+      return { ...state, closeModalAddSai };
+    },
+    setCloseModalEditSai: (state, action) => {
+      const closeModalEditSai = action.payload;
+      return { ...state, closeModalEditSai };
+    },
+    setAddedSais: (state, action) => {
+      const sai = action.payload;
+      return { ...state, addedSais: sai };
+    },
+  },
+});
+
+export const { setCloseModalAddSai, setAddedSais, setCloseModalEditSai } = saisSlice.actions;
+
+export default saisSlice.reducer;
