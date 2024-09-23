b744a02b1d080efbccbb70b7f09a4d24d642f066
Arnel <arnelcariaga@hotmail.com>
2024-09-23 13:17:30 -0400

    Session Provider for main route

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import LeftSidebar from "@/components/LeftSidebar";
 import Navbar from "@/components/Navbar";
 import { auth } from "@/auth";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { SessionProvider } from "next-auth/react";
 
 export default async function MainLayout({
     children,
@@ -10,16 +11,18 @@ export default async function MainLayout({
 }>) {
     const session = await auth()
     return (
-        <div className="flex h-screen ">
-            <LeftSidebar isSidebarOpen={false} />
+        <SessionProvider>
+            <div className="flex h-screen ">
+                <LeftSidebar isSidebarOpen={false} />
 
-            <div className="flex-1 flex flex-col overflow-hidden">
-                <Navbar session={session} />
+                <div className="flex-1 flex flex-col overflow-hidden">
+                    <Navbar session={session} />
 
-                <ScrollArea className="flex-1 overflow-y-auto p-2">
-                    {children}
-                </ScrollArea>
+                    <ScrollArea className="flex-1 overflow-y-auto p-2">
+                        {children}
+                    </ScrollArea>
+                </div>
             </div>
-        </div>
+        </SessionProvider>
     );
 }
