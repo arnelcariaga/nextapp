c6c97e33922e5d09ca634d5d209f39196dd4365e
Arnel <arnelcariaga@hotmail.com>
2024-11-14 15:08:18 -0400

    New features

--- components/SignInElement/signInServerFunc.ts
+++ components/SignInElement/signInServerFunc.ts
@@ -1,11 +1,12 @@
 "use server";
-import { signIn } from "@/auth";
+import { signIn, unstable_update } from "@/auth";
 import { IUserCredentials } from "@/lib/interfaces";
 import { AuthError } from "next-auth";
 
 export async function signInServerFunc(data: IUserCredentials) {
   try {
     await signIn("credentials", { ...data, redirectTo: "/dashboard" });
+    await unstable_update({})
   } catch (error) {
     if (error instanceof AuthError) {
       return { error: true, message: error.cause?.err?.message };
